6c8dbea3348dd211a580788dea26004de2a871cf
Yarik kedrych <yarik.kedrych9@gmail.com>
2024-06-24 19:08:42 +0300

    make build

---
+++ report/html_report/config.js
@@ -0,0 +1,59 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/Card_with_data-qa_card.png",
+        "test": "../bitmaps_test/20240624-185628/Card_with_data-qa_card.png",
+        "selector": "[data-qa=\"card\"]",
+        "fileName": "Card_with_data-qa_card.png",
+        "label": "Card with data-qa_card",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/product-cards/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -824,
+            "height": -2
+          },
+          "rawMisMatchPercentage": 7.970179115853658,
+          "misMatchPercentage": "7.97",
+          "analysisTime": 41
+        },
+        "diffImage": "../bitmaps_test/20240624-185628/failed_diff_Card_with_data-qa_card.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/Link_with_data-qa_hover.png",
+        "test": "../bitmaps_test/20240624-185628/Link_with_data-qa_hover.png",
+        "selector": "[data-qa=\"card\"]",
+        "fileName": "Link_with_data-qa_hover.png",
+        "label": "Link with data-qa_hover",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/product-cards/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -824,
+            "height": -2
+          },
+          "rawMisMatchPercentage": 6.765672637195122,
+          "misMatchPercentage": "6.77",
+          "analysisTime": 79
+        },
+        "diffImage": "../bitmaps_test/20240624-185628/failed_diff_Link_with_data-qa_hover.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
